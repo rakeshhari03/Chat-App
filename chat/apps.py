b2f290afbc03dd88d5a2aa5c95df818f65d522d1
rakesh hari <rakeshharierv@gmail.com>
2026-02-21 11:20:06 +0530

    chnaged chat/app.py

--- chat/apps.py
+++ chat/apps.py
@@ -5,9 +5,3 @@ class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'chat'
 
-    def ready(self):
-        # Reset all users to offline status when app starts
-        # (but keep their last_seen timestamp intact)
-        from accounts.models import CustomUser
-        
-        CustomUser.objects.update(is_online=False)
